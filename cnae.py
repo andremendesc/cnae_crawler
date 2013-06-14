8de1be9bb96f908cc18958b43e58fbc5742a592d
EC2 Default User <ec2-user@ip-10-243-114-206.ec2.internal>
2013-06-14 09:12:35 +0000

    Trying a webserver

--- cnae.py
+++ cnae.py
@@ -2,22 +2,40 @@
 import urllib
 import urllib2
 import sys
+import web
+import json
 
-parametro = sys.argv[1]
 url = 'http://www.cnae.ibge.gov.br/pesquisa.asp'
+urls_aceitas = (
+    '/(.*)', 'cnae'
+)
+app = web.application(urls, globals())
 
-values = {'pesquisa' : parametro,
-          'sourcepage' : 'index',
-          'tabelabusca' : 'CNAE_201@CNAE 2.1 - Subclasses@0@cnaefiscal@0',
-          'tipoordenacao' : 'C'
-          }
+class cnae:        
+    def GET(self, param):
+        if not param: 
+            return 'Faltou o codigo do cnae!'
+        parametro = param
 
-data = urllib.urlencode(values)
-req = urllib2.Request(url, data)
-response = urllib2.urlopen(req)
-the_page = response.readlines()
+        values = {'pesquisa' : parametro,
+                  'sourcepage' : 'index',
+                  'tabelabusca' : 'CNAE_201@CNAE 2.1 - Subclasses@0@cnaefiscal@0',
+                  'tipoordenacao' : 'C'
+                  }
 
-for url in the_page:
-    if url.find('<TD style="FONT: 8pt \'Verdana\'; COLOR: black; BACKGROUND-COLOR: #ffffff" width=680 height=16>') != -1:
-        print url[106:-15]
+        data = urllib.urlencode(values)
+        req = urllib2.Request(urls_aceitas, data)
+        response = urllib2.urlopen(req)
+        the_page = response.readlines()
+        results = []
+
+        for url in the_page:
+            if url.find('<TD style="FONT: 8pt \'Verdana\'; COLOR: black; BACKGROUND-COLOR: #ffffff" width=680 height=16>') != -1:
+                results.append(url[106:-15])
+		web.header('Content-Type', 'application/json')
+		return json.dumps(results)
+
+if __name__ == "__main__":
+    app.run()
+         
 
